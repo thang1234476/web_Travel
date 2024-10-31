1222ba4f34a4a4f8d8485c3bcedbb477784856e6
thang1234476 <nguyenduythang2808@gmail.com>
2024-10-31 17:41:26 +0700

    dt/login-register

---
+++ resources/views/layouts/forgot.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quên mật khẩu</title>
+    <style>
+        .container {
+            width: 600px;
+            height: 350px;
+            min-height: 350px;
+            background: #ccc;
+            border-radius: 10px;
+            overflow: hidden;
+            font-family: "Montserrat", sans-serif;
+            margin: 0 auto;
+            margin-top: 8%;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
+</html>
---
+++ resources/views/layouts/reset.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container {
+            width: 600px;
+            height: 350px;
+            min-height: 350px;
+            background: #ccc;
+            border-radius: 10px;
+            overflow: hidden;
+            font-family: "Montserrat", sans-serif;
+            margin: 0 auto;
+            margin-top: 8%;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+    @yield('content')
+    </div>
+</body>
+</html>
